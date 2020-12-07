5438890ffead652201bc2a1073117ee0784c32ff
Danis_XBZM <60910139+zky-rgb@users.noreply.github.com>
2020-12-07 13:35:12 +0800

    Update huffman.h

--- huffman puls/huffman.h
+++ huffman puls/huffman.h
@@ -3,53 +3,44 @@
 #include<vector>
 #include<unordered_map>
 #include<stack>
-//保存数据权值的节点
-struct WeightNode
+#include<list>
+
+struct WNode
 {
     char c;//所保存的字符
     int weight;//权值
-    int heigh;//以其为根的子树的高度 
-    int posection;//自己在容器中的位置（用于数据的插入）
-    WeightNode *lchild,*rchild,*parent;//左右子树，以及父节点
-    WeightNode(const char &c1='\0'):c(c1),weight(1),heigh(1),lchild(NULL),rchild(NULL),parent(NULL){}//\0表示空的节点
-};
-//编码节点
-struct Codedata
-{
-    unsigned int data;//编码
-    int headempty;//0的数量
-    int length;//位数
-    Codedata(int d,int e,int l):data(d),headempty(e),length(l){}
+    bool codenum;//结点对应值
+    WNode *lchild,*rchild;//左右节点
+    WNode *parent;//父节点
+    WNode (const char c1='\0',const int i=1):c(c1),weight(i),lchild(NULL),rchild(NULL){}
+    bool operator < (const WNode & a) const//重载运算符
+	{
+		return weight<a.weight;
+	}
 };
 //Huffman树类,链式结构
 class Huffman 
 {
     public:
-        Huffman() :negative(0), recode(0), Ehead(0), root(NULL) {}
+        Huffman() :root(NULL) {}
     ~Huffman();//析构函数
-    void getMessage(const std::string s);//获取用来建树的字符串
-    void combine(int i);//将两个结点合并
+    void getMessage(const std::string s)//获取用来建树的字符串
+    {
+        HuffmanStr=s;
+    }
     void handling();//计算字符串中的加权
-    void initialization();//huffman树的初始化
-    int maxheigh(int i);//返回最大的树深
-    void coding();//生成编码
-    void HuffmanInsert(WeightNode *p);//将结点插入加入到权值的结合中
-    void Huffmandestroy(WeightNode *r);//销毁二叉树
-    void Encoding();//将字符串转换为编码
-    void Decoding(std::string c,std::string &s);//将编码转换为字符串
+    void coding(std::unordered_map<char,int>& weight);//生成编码
+    void creatCodetable();//生成对应的huffmam
+    void Huffmandestroy(WNode*r);//销毁二叉树
+    void Decoding(std::string &c,std::string &s);//将编码转换为字符串
     void OutputCode(std::string &code);//输出编码
     void OutputStr();//输出字符串
-    void PrintHuffmanTree(WeightNode* T,int level=1);
     void PrintHuffmanTree();
     friend void getHuffman();
     private:
-    int negative;//负值数
-    unsigned long int recode;//最终的编码
-    int Ehead;//开头零的数目
-    std::vector<WeightNode *> v;//用来保存huffman树的子树们
-    std::unordered_map<char,WeightNode*> m;//用哈希表来建立字典来搜索已有字符
-    std::unordered_map<char,Codedata*> cm;//用来查询编码
+    std::vector<bool> encode;//最终编码
+    std::unordered_map<char,std::vector<bool> > CharCode;//用来存储每个字符所对应的编码
+    std::unordered_map<std::vector<bool>,char> CodeChar;//反向编码的表
     std::string HuffmanStr;//用来接收字符串
-    std::string Huffmancode;//用来接收编码
-    WeightNode *root;//huffman树的根节点
+    WNode *root;//huffman树的根节点
 };
