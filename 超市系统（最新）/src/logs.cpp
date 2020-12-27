8b9358641a672515f4b3c980d5a38aa2bf9c32f1
Danis_XBZM <60910139+zky-rgb@users.noreply.github.com>
2020-12-27 13:13:57 +0800

    Update logs.cpp

--- 超市系统（最新）/src/logs.cpp
+++ 超市系统（最新）/src/logs.cpp
@@ -1,36 +1,32 @@
-#include"Handle.h"
-Logs ::Logs() {
-    string example = "时间 货物名称 货物数量";
-}
+#include"Logs.h"
+#include<fstream>
+#include<string>
 
-void Logs ::ShowLogs(int *id,int *num,int *year,int *month,int i) {
-    /*cout<<"----日志----"<<endl;
-    for(int i=0;i<ids.size();i++){
-        cout<<ids[i]<<" "<<nums[i]<<" "<<years[i]<<" "<<months[i]<<endl;
-    }
-    cout<<"------------"<<endl;*/
+using namespace std;
+
+void Logs::ShowLogs(int* id, int* num, int* year, int* month, int i) {
+
     *id = ids[i];
     *num = nums[i];
     *year = years[i];
     *month = months[i];
 }
 
-void Logs ::SaveLogs() {
-    std::ofstream out("Logs.txt",std::ios::out);
-    for(int i=0;i<ids.size();i++){
-        out<<ids[i]<<" "<<nums[i]<<" "<<years[i]<<" "<<months[i]<<endl;
+void Logs::SaveLogs() {
+    std::ofstream out("Logs.txt", std::ios::out);
+    for (int i = 0; i < ids.size(); i++) {
+        out << ids[i] << " " << nums[i] << " " << years[i] << " " << months[i] << endl;
     }
-    cout<<"Saved to file!"<<endl;
 }
 
-void Logs ::InputLog(int id,int num,int year,int month) {
+void Logs::InputLog(int id, int num, int year, int month) {
     ids.push_back(id);
     nums.push_back(num);
     years.push_back(year);
     months.push_back(month);
 }
 
-int Logs ::ReSize() {
+int Logs::ReSize() {
     return ids.size();
 }
 
